3e7294062bad627ef7ff7468197f1dfc8f0b33b4
Georgi Christov <ga.christov@gmail.com>
2024-03-19 19:02:05 +0100

    Fix iOS tests

--- appIos/feed/tests/FeedTestHostApp.swift
+++ appIos/feed/tests/FeedTestHostApp.swift
@@ -15,6 +15,7 @@ struct FeedTestHostApp: App {
     private let redecorateSectionedFeedUseCase: RedecorateSectionedFeedUseCase
     
     init() {
+        MplNewsfeedDi.shared.setup(appModules: [])
         // Mock/fake necessary constructs based on launch environment
         self.feedPages = FeedType.obtainFromEnvironment()
         self.feedPageCache = FeedCacheType.obtainFromEnvironment()
