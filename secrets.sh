3c6720e470c9bb312d5eb82a5e09d9aa973c619b
Georgi Christov <ga.christov@gmail.com>
2024-03-21 15:42:23 +0100

    Fix concurrent secrets writes

--- secrets.sh
+++ secrets.sh
@@ -2,6 +2,6 @@
 set -e
 # Exports required CI environment secrets to local secrets so that the project can use them
 
-# network credentials
-echo GUARDIAN_API_KEY="$GUARDIAN_API_KEY" >> ./multiplatform/common/network/secrets.properties
+# network credentials (the > is intentional to support concurrent writes)
+echo GUARDIAN_API_KEY="$GUARDIAN_API_KEY" > ./multiplatform/common/network/secrets.properties
 echo GUARDIAN_API_URL="$GUARDIAN_API_URL" >> ./multiplatform/common/network/secrets.properties
